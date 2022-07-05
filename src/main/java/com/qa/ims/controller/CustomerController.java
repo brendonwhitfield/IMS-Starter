5eebff2aa1a62423b16387da56f8c0f4085e776f
brendonwhitfield <brendonwhitfield.bjw@gmail.com>
2022-07-05 21:55:17 +0100

    completed item table full functionality

--- src/main/java/com/qa/ims/controller/CustomerController.java
+++ src/main/java/com/qa/ims/controller/CustomerController.java
@@ -58,12 +58,12 @@ public class CustomerController implements CrudController<Customer> {
 	@Override
 	public Customer update() {
 		LOGGER.info("Please enter the id of the customer you would like to update");
-		Long id = utils.getLong();
+		Long customerId = utils.getLong();
 		LOGGER.info("Please enter a first name");
 		String firstName = utils.getString();
 		LOGGER.info("Please enter a surname");
 		String surname = utils.getString();
-		Customer customer = customerDAO.update(new Customer(id, firstName, surname));
+		Customer customer = customerDAO.update(new Customer(customerId, firstName, surname));
 		LOGGER.info("Customer Updated");
 		return customer;
 	}
@@ -76,8 +76,8 @@ public class CustomerController implements CrudController<Customer> {
 	@Override
 	public int delete() {
 		LOGGER.info("Please enter the id of the customer you would like to delete");
-		Long id = utils.getLong();
-		return customerDAO.delete(id);
+		Long customerId = utils.getLong();
+		return customerDAO.delete(customerId);
 	}
 
 }
